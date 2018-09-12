32554c65b92e264ab236c1e07e7c89b90a02b0f5
20161104568 <a20161104568@Mac02.local>
2018-09-12 11:16:57 +0800

    输出1到100
    输出1到100

---
+++ L02.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+let  x=2;
+let y=14;
+print(x+y);
+for i in 1...100{
+    print(i);
+}
+
+var str = "Hello, playground"
